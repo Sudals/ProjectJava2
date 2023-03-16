08b5ed32c558b4f2fb810beed052ff939e450017
Sudals <ubc445@naver.com>
2023-03-16 11:00:28 +0900

    Weak1_UBC

---
+++ Games/src/BeatBox/BeatClass.java
@@ -0,0 +1,15 @@
+package BeatBox;
+import javax.swing.JFrame;
+public class BeatClass extends JFrame{
+    public  BeatClass(){
+        setTitle("BeatBox");
+        setSize(Main.Width,Main.Height);
+        setResizable(false);
+        setLocationRelativeTo((null));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+
+}
