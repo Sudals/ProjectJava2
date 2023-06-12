6b5edcc13096500739ccf3d2b1af7776c588f666
ssreo <127308840+ssreo@users.noreply.github.com>
2023-06-12 20:02:31 +0900

    Update KeyListener.java_0612_Seongwon
    add keypad sound

--- Games/src/BeatBox/KeyListener.java
+++ Games/src/BeatBox/KeyListener.java
@@ -1,13 +1,29 @@
 package BeatBox;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 
 public class KeyListener extends KeyAdapter {
+
     private BeatClass beatClass;
     public KeyListener(BeatClass beatClass) {
         this.beatClass = beatClass;
+        try {
+            // 효과음 파일 로드
+            soundClip = AudioSystem.getClip();
+            soundClip.open(AudioSystem.getAudioInputStream(Main.class.getResource("../Resources/notepadSound.wav")));
+        } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    private Clip soundClip; 
+
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -45,7 +61,22 @@ public class KeyListener extends KeyAdapter {
                 beatClass.getCardLayout().show(beatClass.getContentPanel(), "gameWindows"); // 게임호면으로 복귀
             }
         }
+        if (e.getKeyCode() == KeyEvent.VK_A ||
+                e.getKeyCode() == KeyEvent.VK_S ||
+                e.getKeyCode() == KeyEvent.VK_D ||
+                e.getKeyCode() == KeyEvent.VK_J ||
+                e.getKeyCode() == KeyEvent.VK_K ||
+                e.getKeyCode() == KeyEvent.VK_L) {
+            playSoundEffect();
+        }
+    }private void playSoundEffect() {
+        if (soundClip.isRunning()) {
+            soundClip.stop(); 
+        }
+        soundClip.setFramePosition(0); 
+        soundClip.start(); 
     }
+
 
     @Override
     public void keyReleased(KeyEvent e) {
